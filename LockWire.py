25b0f532918d20eeea988433940b58421c3dd4f4
CRAWNiiK <123910716+CRAWNiiK@users.noreply.github.com>
2023-03-27 22:57:01 -0400

    Update LockWire.py

--- LockWire.py
+++ LockWire.py
@@ -1,71 +1,63 @@
-import rsa
+import argparse
 import os
+import rsa
 
-def generate_key():
+def generate_key(keyfile):
     (public_key, private_key) = rsa.newkeys(4096)
-    return (public_key, private_key)
+    with open(keyfile + "_pub.pem", "wb") as f:
+        f.write(public_key.save_pkcs1())
+    with open(keyfile + "_priv.pem", "wb") as f:
+        f.write(private_key.save_pkcs1())
+    print("New key pair generated.")
 
-def save_keypair():
-    (pubkey, privkey) = generate_key()
-    with open("key_pub.txt", "w") as f:
-        f.write(pubkey.save_pkcs1().decode('utf-8'))
-    with open("key_private.txt", "w") as f:
-        f.write(privkey.save_pkcs1().decode('utf-8'))
-
-def load_keypair():
-    with open("key_pub.txt", "r") as f:
-        pubdata = f.read().encode('utf-8')
-        pubkey = rsa.PublicKey.load_pkcs1(pubdata)
-    with open("key_private.txt", "r") as f:
-        privdata = f.read().encode('utf-8')
-        privkey = rsa.PrivateKey.load_pkcs1(privdata)
-    return (pubkey, privkey)
-
-def encrypt_message(pubkey, message):
-    crypto = rsa.encrypt(message.encode('utf-8'), pubkey)
-    with open("encrypted_message.txt", "wb") as f:
+def encrypt(keyfile, message_file, output_file):
+    with open(keyfile + "_pub.pem", "rb") as f:
+        pubkey_data = f.read()
+    pubkey = rsa.PublicKey.load_pkcs1(pubkey_data)
+    with open(message_file, "rb") as f:
+        message = f.read()
+    crypto = rsa.encrypt(message, pubkey)
+    with open(output_file, "wb") as f:
         f.write(crypto)
-    return crypto
+    print("Message encrypted successfully.")
+    print(f"Encrypted message saved to {output_file}")
 
-def decrypt_message(privkey, crypto):
-    message = rsa.decrypt(crypto, privkey).decode('utf-8')
-    with open("decrypted_message.txt", "w") as f:
+def decrypt(keyfile, message_file, output_file):
+    with open(keyfile + "_priv.pem", "rb") as f:
+        privkey_data = f.read()
+    privkey = rsa.PrivateKey.load_pkcs1(privkey_data)
+    with open(message_file, "rb") as f:
+        crypto = f.read()
+    message = rsa.decrypt(crypto, privkey)
+    with open(output_file, "wb") as f:
         f.write(message)
-    return message
+    print("Message decrypted successfully.")
+    print(f"Decrypted message saved to {output_file}")
 
-while True:
-    choice = input("Press 1 for encryption or press 2 for decryption: ")
+def main():
+    parser = argparse.ArgumentParser(description="Encrypt or decrypt a message using RSA 4096-bit.")
+    parser.add_argument("-g", "--generate-key", action="store_true", help="Generate a new key pair.")
+    parser.add_argument("-e", "--encrypt", metavar="FILE", help="Encrypt a message from the specified input file.")
+    parser.add_argument("-d", "--decrypt", metavar="FILE", help="Decrypt a message from the specified input file.")
+    parser.add_argument("-k", "--keyfile", default="key", help="Specify the name of the key file (default: key).")
+    parser.add_argument("-o", "--output-file", help="Specify the output file.")
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s 2.0")
+    args = parser.parse_args()
 
-    if choice == '1':
-        option = input("Press 1 to generate new keypair or press 2 to use existing keypair: ")
-        if option == '1':
-            save_keypair()
-            print("Keypair generated successfully!")
-        elif option == '2':
-            try:
-                pubkey, privkey = load_keypair()
-            except:
-                print("Unable to load keypair, please generate a new keypair.")
-                continue
-            message = input("Enter the message to be encrypted: ")
-            crypto = encrypt_message(pubkey, message)
-            print("Message encrypted successfully!")
-            print(f"Encrypted message saved to encrypted_message.txt")
-    elif choice == '2':
-        try:
-            pubkey, privkey = load_keypair()
-        except:
-            print("Unable to load keypair, please generate a new keypair.")
-            continue
-        with open("encrypted_message.txt", "rb") as f:
-            crypto = f.read()
-        message = decrypt_message(privkey, crypto)
-        print("Message decrypted successfully!")
-        print(f"Decrypted message saved to decrypted_message.txt")
+    if args.generate_key:
+        generate_key(args.keyfile)
+    elif args.encrypt:
+        if not args.output_file:
+            print("Error: Output file not specified.")
+            return
+        encrypt(args.keyfile, args.encrypt, args.output_file)
+    elif args.decrypt:
+        if not args.output_file:
+            print("Error: Output file not specified.")
+            return
+        decrypt(args.keyfile, args.decrypt, args.output_file)
     else:
-        print("Invalid input. Please try again.")
-        continue
+        parser.print_help()
 
-    choice = input("Press 1 for encryption or press 2 for decryption. Press any other key to exit: ")
-    if choice != '1' and choice != '2':
-        break
+if __name__ == "__main__":
+    main()
